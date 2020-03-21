ea77ee38e8c30f28f7a17b4261de9826d56d20b7
hmsfeng <hmsfeng@163.com>
2020-03-22 02:52:26 +0800

    修改编译脚本
    修改build_imx6ull.sh

--- build_imx6ull.sh
+++ build_imx6ull.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- distclean
-#make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- mx6ull_zdyz_emmc_defconfig
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- menuconfig
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- -j8
+#make mx6ull_zdyz_emmc_defconfig
+make menuconfig
+make -j8
